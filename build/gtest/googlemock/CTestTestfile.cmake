a6a5d313d6ea67a4732ec8f8522af1fad467a2ee
Slender- <56681199+slender1031@users.noreply.github.com>
2023-03-17 10:41:54 +0800

    Delete build directory

--- build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/slender/torch_ws/deeplab_test_ros/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/test_pytorch/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/slender/torch_ws/deeplab_test_ros/src/test_pytorch
-# Build directory: /home/slender/torch_ws/deeplab_test_ros/build/test_pytorch
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
